d0223eb8783d72cab2ac8c6cb4d7b4f43f4f5b7a
tamarasisvadze <tamarrisisvadze02@gmail.com>
2021-04-02 18:39:55 +0400

    first commit

---
+++ alarmpython.py
@@ -0,0 +1,43 @@
+from tkinter import *
+import datetime
+import time
+import winsound
+
+def alarm(set_alarm_timer):
+    while True:
+        time.sleep(1)
+        currect_time = datetime.datetime.now
+        now = currect_time.strftime("%H:%M:%S")
+        date = currect_time.strftime("%d/%m/%Y")
+        print("The Set Date is:",date)
+        print(now)
+        if now == set_alarm_timer:
+            print("Time to Wake up")
+        winsound.PlaySound("sound.wav".winsound.SND_ASYNC)
+        break
+
+def actual_time():
+    set_alarm_timer = f"{hour.get()}:{min.get()}:{sec.get()}"
+    alarm(set_alarm_timer)
+
+clock = Tk()
+clock.title("DataFlair Alarm Clock")
+clock.geometry("400x200")
+time_format=Label(clock, text= "Enter time in 24 hour format!", fg="red",bg="black",font="Arial").place(x=60,y=120)
+addTime = Label(clock,text = "Hour  Min   Sec",font=60).place(x = 110)
+
+# The Variables we require to set the alarm(initialization):
+hour = StringVar()
+min = StringVar()
+sec = StringVar()
+
+#Time required to set the alarm clock:
+hourTime= Entry(clock,textvariable = hour,bg = "pink",width = 15).place(x=110,y=30)
+minTime= Entry(clock,textvariable = min,bg = "pink",width = 15).place(x=150,y=30)
+secTime = Entry(clock,textvariable = sec,bg = "pink",width = 15).place(x=200,y=30)
+
+#To take the time input by user:
+submit = Button(clock,text = "Set Alarm",fg="red",width = 10,command = actual_time).place(x =110,y=70)
+
+clock.mainloop()
+#Execution of the window.
